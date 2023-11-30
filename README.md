467cb62803984d03f355f272a0bd34b26783508c
Firat Ozdemir <fozdemir@gmail.com>
2023-11-30 14:39:31 +0100

    dall-e_firat

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # gen-ai-sdsc-hackathon
+
+
+Instructions for using scripts from Firat:   
+All scripts look for a specific file that won't be on github that contain tokens for corresponding APIs. 
+Please create corresponding files in your own deployment with your own tokens/keys. 
+
+Otherwise, you can use `environment-firat.yml` to install the conda environment to get these scripts working. 
---
+++ prompt_generator_for_gpt.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+def prompt_string(gender = 'woman', ethnicity = 'korean', age = '50s', destination = 'Sahara', weather_lims = [35, 40], with_text_output=False):
+
+    string_visual = f'A {gender}, {ethnicity}, in their {age} is flying to {destination}. The weather will be between {np.min(weather_lims):.1f} to {np.max(weather_lims):.1f} degrees celsius. Draw a hyper realistic picture of this person with clothing that is fitting for that weather. Make sure this person is facing the viewer. Make sure the surrounding of this person is fitting of the destionation:{destination}.' 
+    if with_text_output: 
+        string_text = f'As text output, list the outer set of clothes with basic descriptions (type of clothing, 2-3 word description) that would be fitting for searching this clothings online. Make sure that the text output is **only** a csv compatible output and no other text.'
+        string_prompt = string_visual + string_text
+    else:
+        string_prompt = string_visual
+    return string_prompt
