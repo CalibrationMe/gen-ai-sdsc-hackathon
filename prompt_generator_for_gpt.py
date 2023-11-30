2f451f0ff02f559cad1c3c3e03e3600283387d8a
Firat Ozdemir <fozdemir@gmail.com>
2023-11-30 22:16:49 +0100

    replace flying with travelling to prevent confusion

--- prompt_generator_for_gpt.py
+++ prompt_generator_for_gpt.py
@@ -4,7 +4,7 @@ import numpy as np
 
 def prompt_string(gender = 'woman', ethnicity = 'korean', age = '50s', destination = 'Sahara', weather_lims = [35, 40], with_text_output=False):
 
-    string_setup = f'A {gender}, {ethnicity}, aged {age} is flying to {destination}. The weather will be between {np.min(weather_lims):.1f} to {np.max(weather_lims):.1f} degrees celsius. '
+    string_setup = f'A {gender}, {ethnicity}, aged {age} is travelling to {destination}. The weather will be between {np.min(weather_lims):.1f} to {np.max(weather_lims):.1f} degrees celsius. '
     string_visual = f'Draw a hyper realistic picture of this person with clothing that is fitting for that weather. Make sure this person is facing the viewer. Make sure the surrounding of this person is fitting of the destionation:{destination}.' 
     if with_text_output: 
         string_text = f'As text output, list the outer set of clothes with basic descriptions (type of clothing, 2-3 word description) that would be fitting for searching this clothings online. Make sure that the text output is **only** a csv compatible output and no other text.'
@@ -15,7 +15,7 @@ def prompt_string(gender = 'woman', ethnicity = 'korean', age = '50s', destinati
 
 
 def prompt_only_text(gender = 'man', ethnicity = 'european', age = '30s', destination = 'North Pole', weather_lims = [-35, -20]):
-    string_setup = f'A {gender}, {ethnicity}, aged {age} is flying to {destination}. The weather will be between {np.min(weather_lims):.1f} to {np.max(weather_lims):.1f} degrees celsius. '
+    string_setup = f'A {gender}, {ethnicity}, aged {age} is travelling to {destination}. The weather will be between {np.min(weather_lims):.1f} to {np.max(weather_lims):.1f} degrees celsius. '
     
     string_text = f'As text output, list the outer set of clothes with basic descriptions (type of clothing, 2-3 word description) that would be fitting for searching this clothings online. Make sure that the text output is **only** a csv compatible output and no other text.'
     
